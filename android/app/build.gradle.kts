e496b736141ec6e2c774a9f7fbf26ee25c5dc1a3
iMankoppai <248448998@qq.com>
2026-09-18 00:09:38 +0800

    Release Android v1.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.imankoppai.mediaanvil"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.4"
+        versionCode = 13
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
